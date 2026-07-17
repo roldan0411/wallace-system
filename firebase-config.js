b7ebb1cd6f7d362a1bb09f99ad41f3a69a245d39
roldan0411 <144633382+roldan0411@users.noreply.github.com>
2026-07-17 14:19:26 -0500

    Change Firebase configuration values
    Updated Firebase configuration for Wallace POS project.

--- firebase-config.js
+++ firebase-config.js
@@ -2,10 +2,10 @@
 // Reemplaza estos valores con los de tu proyecto Firebase.
 window.FIREBASE_CONFIG = {
   apiKey: "TU_API_KEY",
-  authDomain: "wallace-pos.firebaseapp.com",
-  databaseURL: "https://wallace-pos-default-rtdb.firebaseio.com",
-  projectId: "wallace-pos",
-  storageBucket: "wallace-pos.firebasestorage.app",
+  authDomain: "wallace-system.firebaseapp.com",
+  databaseURL: "https://wallace-system-default-rtdb.firebaseio.com",
+  projectId: "wallace-system",
+  storageBucket: "wallace-system.firebasestorage.app",
   messagingSenderId: "TU_SENDER_ID",
   appId: "TU_APP_ID"
 };
