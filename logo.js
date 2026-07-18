953d7f7f227296f9fbcb55510b1f647ae458f45d
roldan0411 <144633382+roldan0411@users.noreply.github.com>
2026-07-18 16:02:13 -0500

    Delete logo.js

--- logo.js
+++
@@ -1,3 +0,0 @@
-// Logo por defecto de Wallace POS (se puede cambiar por negocio desde el panel).
-// Reemplaza con tu logo en base64: window.LOGO_DEFAULT='data:image/png;base64,....';
-window.LOGO_DEFAULT = '';
